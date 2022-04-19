08670fde41714b34a522710df25e6c649532f121
kubasmsjk <jakm5000@wp.pl>
2022-04-19 22:54:50 +0200

    modyfikacja panelu admina

--- GamePLAY/admin.py
+++ GamePLAY/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
-from .models import Player,Team,Match
+from .models import Player, Team, Match
+
+
 # Register your models here.
 
-admin.site.register(Player)
-admin.site.register(Team)
-admin.site.register(Match)
+class PlayerInLIne(admin.TabularInline):
+    model=Player
+
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'player_position', 'team']
+    list_filter = ['team']
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['team_name', 'stadium_name', 'coach_name', 'creation_date']
+    inlines = [
+        PlayerInLIne
+    ]
+
+
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    search_fields = ['home_team__team_name','visiting_team__team_name']
+    list_display = ['home_team', 'visiting_team', 'match_date', 'queue_number', 'match_score']
--- GamePLAY/models.py
+++ GamePLAY/models.py
@@ -1,8 +1,8 @@
 import pytz as pytz
 from django.db import models
 
-class Team(models.Model):
 
+class Team(models.Model):
     team_name = models.CharField(max_length=50)
     stadium_name = models.CharField(max_length=50)
     coach_name = models.CharField(max_length=50)
@@ -12,8 +12,11 @@ class Team(models.Model):
         verbose_name = "Team"
         verbose_name_plural = "Teams"
 
-class Player(models.Model):
+    def __str__(self):
+        return self.team_name
 
+
+class Player(models.Model):
     PLAYER_POSITION = [
         ('Go', 'Goalkeeper'),
         ('De', 'Defender'),
@@ -21,30 +24,32 @@ class Player(models.Model):
         ('St', 'Striker'),
     ]
 
-    first_name = models.CharField(max_length=50,default='')
-    last_name = models.CharField(max_length=50,default='')
+    first_name = models.CharField(max_length=50, default='')
+    last_name = models.CharField(max_length=50, default='')
     date_of_birth = models.DateField(null=True)
-    player_position = models.CharField(max_length=10, choices=PLAYER_POSITION,default='')
-    country = models.CharField(max_length=50, choices=pytz.country_names.items(),blank=True)
+    player_position = models.CharField(max_length=10, choices=PLAYER_POSITION, default='')
+    country = models.CharField(max_length=50, choices=pytz.country_names.items(), blank=True)
     number_of_goals = models.IntegerField(default=0)
 
-    team = models.ForeignKey(Team, on_delete=models.CASCADE,default='')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, default='')
 
     class Meta:
         verbose_name = "Player"
         verbose_name_plural = "Players"
 
-class Match(models.Model):
+    def __str__(self):
+        return str(self.first_name) + ' ' + str(self.last_name)
 
+class Match(models.Model):
     home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team')
     visiting_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='visiting_team')
     match_date = models.DateField(null=True)
     queue_number = models.IntegerField()
-    match_score = models.CharField(max_length=5)
+    match_score = models.CharField(max_length=5, blank=True)
 
     class Meta:
         verbose_name = "Match"
         verbose_name_plural = "Matches"
 
-
-
+    def __str__(self):
+        return self.home_team
