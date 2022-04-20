4f02080e8962450349a78a224aeb20c683a7719c
Bartosz Kraśniewski <kwachu2234@gmail.com>
2022-04-20 16:09:01 +0200

    liga miszczow

--- GamePLAY/admin.py
+++ GamePLAY/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Player, Team, Match
+from .models import Player, Team, Match, League
 
 
 # Register your models here.
 
 class PlayerInLIne(admin.StackedInline):
     model=Player
+
+
+class TeamsInLine(admin.StackedInline):
+    model=Team
 
 
 
@@ -28,3 +32,7 @@ class TeamAdmin(admin.ModelAdmin):
 class MatchAdmin(admin.ModelAdmin):
     search_fields = ['home_team__team_name','visiting_team__team_name']
     list_display = ['home_team', 'visiting_team', 'match_date', 'queue_number', 'match_score']
+
+@admin.register(League)
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ['league_name']
--- GamePLAY/models.py
+++ GamePLAY/models.py
@@ -1,12 +1,12 @@
 import pytz as pytz
 from django.db import models
 
-
 class Team(models.Model):
     team_name = models.CharField(max_length=50)
     stadium_name = models.CharField(max_length=50)
     coach_name = models.CharField(max_length=50)
     creation_date = models.DateField(null=True)
+
 
     class Meta:
         verbose_name = "Team"
@@ -14,6 +14,26 @@ class Team(models.Model):
 
     def __str__(self):
         return self.team_name
+
+class League(models.Model):
+    league_name = models.CharField(max_length=50)
+
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team1', default='')
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team2', default='')
+    team3 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team3', default='')
+    team4 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team4', default='')
+    team5 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team5', default='')
+    team6 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team6', default='')
+    team7 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team7', default='')
+    team8 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team8', default='')
+
+    class Meta:
+        verbose_name = "League"
+        verbose_name_plural = "Leagues"
+
+    def __str__(self):
+        return str(self.league_name)
+
 
 
 class Player(models.Model):
@@ -43,13 +63,13 @@ class Player(models.Model):
 class Match(models.Model):
     home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team')
     visiting_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='visiting_team')
-    match_date = models.DateField(null=True)
+    match_date = models.DateTimeField(null=True)
     queue_number = models.IntegerField()
-    match_score = models.CharField(max_length=5, blank=True)
+    match_score = models.CharField(max_length=5, blank=True, default='-')
 
     class Meta:
         verbose_name = "Match"
         verbose_name_plural = "Matches"
 
     def __str__(self):
-        return self.home_team
+        return str(self.home_team)
