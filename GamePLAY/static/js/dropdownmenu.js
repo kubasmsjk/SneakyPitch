86ed1884a84233d6506d86d8c3399ff643636516
kubasmsjk <jakm5000@wp.pl>
2022-05-04 17:12:20 +0200

    usuniecie crips forms

--- GamePLAY/static/js/dropdownmenu.js
+++ GamePLAY/static/js/dropdownmenu.js
@@ -2,6 +2,12 @@
 toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
+  let li = document.createElement('li');
+        li.textContent = 'About Us';
+        // add it to the ul element
+        menu.appendChild(li);
+
+        console.log(menu.innerHTML);
 }
 
 // Close the dropdown menu if the user clicks outside of it
--- GamePLAY/views.py
+++ GamePLAY/views.py
@@ -2,29 +2,36 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 
+
 # Create your views here.
 def mainView(request):
     return render(request, 'main.html')
 
+
 def tablesView(request):
-    team_objects = Team.objects.all()
-    dane = {'team_objects' : team_objects }
-    return render(request, 'tables.html', dane)
+    team_objects = Team.objects.all().order_by('-points')
+    context = {
+        'team_objects': team_objects
+    }
+    return render(request, 'tables.html', context)
+
 
 def queuesView(request):
     return render(request, 'queues.html')
 
+
 def teamsView(request):
     team_objects = Team.objects.all()
     player_objects = Player.objects.all()
-    dane_team = { 'team_objects' : team_objects,
-                  'player_objects': player_objects }
+    dane_team = {'team_objects': team_objects,
+                 'player_objects': player_objects}
 
     return render(request, 'teams.html', dane_team)
+
 
 def shootersRankView(request):
     player_rank = Player.objects.all().order_by('-number_of_goals')[:10]
     context = {
         'player_rank': player_rank
     }
-    return render(request, 'shootersRank.html',context)
+    return render(request, 'shootersRank.html', context)
