0a098ca8a4d2398a10e7cd0587a892351fd0e0cf
Leckly <minejoyjoy@gmail.com>
2022-05-18 18:38:02 +0200

    logowanie na mainie, dzialajace

--- GamePLAY/views.py
+++ GamePLAY/views.py
@@ -95,26 +95,22 @@ def registerView(request):
 #         messages.error(request,"Unsuccessful registration. Invalid information.")
 #     form = NewUserForm()
 #     return render(request=request, template_name="main.html", context={"register_form":form})
-#
-# def login_request(request):
-#     if request.method == "POST":
-#         form = AuthenticationForm(request, data = request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 messages.info(request, f"You are now logged in as {username}.")
-#                 return redirect("main")
-#             else:
-#                 messages.error(request,"Invalid username or password.")
-#         else:
-#             messages.error(request,"Invalid username or password.")
-#     form = AuthenticationForm()
-#     return render(request=request, template_name="login.html", context={"login_form":form})
-#
-# def logout_request(request):
-#     logout(request)
-#     messages.info(request,"You have successfully logged out.")
-#     return redirect("main")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("main")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"login_form":form})
+
