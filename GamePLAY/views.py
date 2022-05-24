17e7a115e51e1790f0cb9074353613ced0086f47
kubasmsjk <jakm5000@wp.pl>
2022-05-24 13:00:21 +0200

    naprawa plikow

--- GamePLAY/views.py
+++ GamePLAY/views.py
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
 from .forms import SearchForm
 from .models import *
 from .models import Search
-from django.contrib.auth import login, authenticate
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 from django.core.mail import send_mail
 import folium
 import geocoder
-=======
-from django.shortcuts import render
-from .models import *
-from django.core.mail import send_mail
 from django.contrib import messages
->>>>>>> c6bc06a117a85235b167be8f9a42765bfd07e4f1
-=======
-from django.shortcuts import render
-from .models import *
-from django.core.mail import send_mail
-from django.contrib import messages
->>>>>>> c6bc06a117a85235b167be8f9a42765bfd07e4f1
 
 
 # Create your views here.
@@ -57,13 +40,6 @@ def main_view(request):
             messages.success(request, "Fields cannot be empty")
         else:
             send_mail(subject, message, email, ['jakm5000@wp.pl'])
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> c6bc06a117a85235b167be8f9a42765bfd07e4f1
-=======
->>>>>>> c6bc06a117a85235b167be8f9a42765bfd07e4f1
     return render(request, 'main.html', dane_items)
 
 
